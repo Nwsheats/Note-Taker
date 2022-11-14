320ee1b56f4542ea7be7ffed52ab7d806b26ddb7
Nwsheats <nwsheats@gmail.com>
2022-11-14 13:19:41 -0600

    added router api paths

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,13 +2,30 @@ const path = require('path');
 const router = require('express').Router();
 
 
-//router.get
+router.get('/', (req, res) => {
+    res.send('Landing Page')
+});
+
+router.get('/api/notes', (req, res) => {
+    res.ReadFile('db.json')
+});
 
 
 //router.post
 
+router.post('/api/notes', (req, res) => {
+    res.appendFile('db.json')
+})
+
 //router.delete
 
+router.delete('/api/notes/:id', (req, res) => {
+    res.send(`Delete User with ID ${req.params.id}`)
+})
+
+router.param("id", (req, res, next, id) => {
+    console.log(id)
+});
 
 
 module.exports = router;
