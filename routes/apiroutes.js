72d5b458b23e583a6f6b14cfe7eeec5d2f41f246
Nwsheats <nwsheats@gmail.com>
2022-11-14 18:32:48 -0600

    added uuid and cleaned up some api routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,25 +1,28 @@
 const path = require('path');
 const router = require('express').Router();
+const fs = require('fs')
 
 
 router.get('/', (req, res) => {
     res.send('Landing Page')
 });
 
-router.get('/api/notes', (req, res) => {
-    res.ReadFile('db.json')
+router.get('/notes', (req, res) => {
+    res.readFromFile('./db/db.json').then((data) =>
+    res.json(JSON.parse(data))('db.json')
+    );
 });
 
 
 //router.post
 
-router.post('/api/notes', (req, res) => {
+router.post('/notes', (req, res) => {
     res.appendFile('db.json')
 })
 
 //router.delete
 
-router.delete('/api/notes/:id', (req, res) => {
+router.delete('/notes/:id', (req, res) => {
     res.send(`Delete User with ID ${req.params.id}`)
 })
 
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,9 +2,6 @@ const path = require('path');
 const router = require('express').Router();
 
 //router.get
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
-});
 
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
