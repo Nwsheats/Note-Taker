f1976356de8aa7224f8f9e732506e568c748b7d9
Nwsheats <nwsheats@gmail.com>
2022-11-15 18:13:53 -0600

    fixed delete functionality and changed id name

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 
-
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-// app.use('/api', api);
+
 
 app.use('/api', apiRoutes)
 app.use('/', htmlRoutes)
